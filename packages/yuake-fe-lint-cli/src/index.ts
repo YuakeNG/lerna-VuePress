3e784252635a267e74014712721b07c2820a0f43
yuake <415425666@qq.com>
2023-08-31 01:28:38 +0800

    feat: 脚手架

--- packages/yuake-fe-lint-cli/src/index.ts
+++ packages/yuake-fe-lint-cli/src/index.ts
@@ -1,11 +1,34 @@
+import ora from 'ora';
 import initAction from './action/init';
-import type { InitOptions } from './type';
+import type { InitOptions,ScanOptions } from './type';
+import scanAction from './action/scan';
+import { PKG_NAME, } from './utils/constants';
 
 type IInitOptions = Omit<InitOptions, 'checkVersionUpdate'>;
 
 export const init = async (options: IInitOptions) => {
+  console.log(options, 'init');
+ 
   return await initAction({
     ...options,
     checkVersionUpdate: false,
   });
+};
+export const scan = async (options: ScanOptions) => {
+  const checking = ora();
+  checking.start(`执行 ${PKG_NAME} 代码检查`);
+
+  const report = await scanAction(options);
+  const { results, errorCount, warningCount } = report;
+  let type = 'succeed';
+  if (errorCount > 0) {
+    type = 'fail';
+  } else if (warningCount > 0) {
+    type = 'warn';
+  }
+
+  checking[type]();
+  if (results.length > 0) printReport(results, false);
+
+  return report;
 };
